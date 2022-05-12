027fbc8876fc0de46cc6a6397992347274aa9e0d
gabrieljuliao <gabrieljuliao>
2022-05-12 16:25:21 -0300

    only root are allowed to run the deployment script

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
 # continers config
 NETWORK_NAME="quotation-network"
 #MySQL
